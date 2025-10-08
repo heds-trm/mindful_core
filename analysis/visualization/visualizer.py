0293aca8bbaab4291378e9680c079b3c0215a2d7
vdurandde <valentin.durand-de-gevigney@hesge.ch>
2025-10-08 11:26:13 +0200

    Fixed two typos from last commit

--- analysis/visualization/visualizer.py
+++ analysis/visualization/visualizer.py
@@ -8,13 +8,14 @@ from pathlib import Path
 from abc import abstractmethod, ABC
 from typing import Literal, Sequence
 
+from mindful_core.utils.imaging import get_3d_image_max_intensity_slice_index, get_center_of_mass
+from mindful_core.utils.visualization import format_image
+from mindful_core.utils.data_constants import SCAN_ID
 from mindful_core.data import ModalitySet, ModalityType, Modality
 from mindful_core.models.module import MindfulModule, ModelInput
 from mindful_core.models.model_output import ClassifierOutput
 from mindful_core.models.classification.abstract_classifier import AbstractClassifier
 from mindful_core.analysis.visualization.radar_chart import radar_from_data_frame
-from mindful_core.utils.imaging import get_3d_image_max_intensity_slice_index, get_center_of_mass
-from mindful_core.utils.visualization import format_image
 
 
 class Visualizer(ABC):
