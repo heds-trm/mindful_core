49619ab7968129ff3299cda5cfdec46fef30fbe6
vdurandde <valentin.durand-de-gevigney@hesge.ch>
2025-12-10 17:04:38 +0100

    Added a very simple autoencoder interface

---
+++ models/autoencoder_interface.py
@@ -0,0 +1,8 @@
+import torch
+from abc import abstractmethod, ABC
+
+class AutoencoderInterface(ABC):
+    @abstractmethod
+    def autoencode(inputs: torch.Tensor | tuple[torch.Tensor]) -> torch.Tensor | tuple[torch.Tensor]:
+        raise NotImplementedError("`autoencode` must be implemented in subclasses.")
+    
