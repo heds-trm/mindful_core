05c838e508d71add470e3d7f163a02bb03aff48f
schmidje <jerome.schmid@gmail.com>
2026-06-07 00:18:48 +0200

    Fix path for writing version to version.py

--- scripts/maintenance/update_version.py
+++ scripts/maintenance/update_version.py
@@ -4,6 +4,6 @@ import tomllib
 with open("pyproject.toml", "rb") as f:
     version = tomllib.load(f)["tool"]["poetry"]["version"]
 
-Path("/version.py").write_text(
+Path("version.py").write_text(
     f'__version__ = "{version}"\n'
 )
